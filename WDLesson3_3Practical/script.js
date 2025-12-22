963ef7f370a66acb43d192c3e6599c62bf31ac3e
crtygdtech <genesisd2790@nycstudents.net>
2025-12-22 23:05:32 +0000

    updated practical lesson 3.3

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -3,5 +3,14 @@ function Flower1(){
     document.getElementById("highlight").innerHTML="Beautiful cherry blossom"
 }
 function Flower2(){
-    
+    document.getElementById("highlight").innerHTML="Dianthus with mix color of red and white"
+}
+function Flower3(){
+    document.getElementById("highlight").innerHTML="Blue hydrangea flowers"
+}
+function Flower4(){
+    document.getElementById("highlight").innerHTML="Pink geranium flowers"
+}
+function Flower5(){
+    document.getElementById("highlight").innerHTML="Purple balloon flower"
 }
