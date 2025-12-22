20c9ceea4a301a7318b6aa983df4505c42f11d9b
crtygdtech <genesisd2790@nycstudents.net>
2025-12-22 14:21:56 +0000

    update 3.3

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,7 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function Flower1(){
+    document.getElementById("highlight").innerHTML="Beautiful cherry blossom"
+}
+function Flower2(){
+    
+}
