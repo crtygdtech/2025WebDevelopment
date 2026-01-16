8d6985028a5cb88c019bfd8d06276718e5d94b22
crtygdtech <genesisd2790@nycstudents.net>
2026-01-16 14:31:34 +0000

    update lesson 3.6

--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -1,5 +1,10 @@
 // Challenge 2:  Create the function for the Average program.  Refer to the resource link in the HTML for assistance.
-
+function average(){
+    let g1 = document.getElementById("g1");
+    let g2 = document.getElementById("g2");
+    let g3 = document.getElementById("g3");
+    
+}
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
 
