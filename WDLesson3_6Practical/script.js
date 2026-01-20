6c61039f140a47e2356f572ec6ad67275f4805d0
crtygdtech <genesisd2790@nycstudents.net>
2026-01-20 14:31:13 +0000

    updated

--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -3,7 +3,9 @@ function average(){
     let g1 = document.getElementById("g1");
     let g2 = document.getElementById("g2");
     let g3 = document.getElementById("g3");
-    
+    let output = document.getElementById("output")
+    let answer = (parseFloat(g1.value + g2.value + g3.value))/3;
+    output.innerHTML = "Average is" + answer;
 }
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
