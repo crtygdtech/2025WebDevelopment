a8fee350050cd45434e1a419f384953a7aaca923
crtygdtech <genesisd2790@nycstudents.net>
2026-01-22 14:33:14 +0000

    update lesson 3.7

--- WDLesson3_7Practical/script.js
+++ WDLesson3_7Practical/script.js
@@ -3,6 +3,15 @@
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
+function distance(){
+      let x1 = parseFloat(document.getElementById("x1").value);
+      let y1 = parseFloat(document.getElementById("y1").value);
+      let x2 = parseFloat(document.getElementById("x2").value);
+      let y2 = parseFloat(document.getElementById("y2").value);
+      let output = document.getElementById("output");
+      let d = Math.sqrt((x2-x1)**2 + (y2-y1)**2);
+      output.innerHTML = "Distance is" + d;
+}
 
 /* Challenge 4: Create a function to serve as the event handler for the compound interest UI. Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 1.
