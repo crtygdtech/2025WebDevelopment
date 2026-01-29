4f4109b8d936cb13bb4c60e71acbc768c761af5a
crtygdtech <genesisd2790@nycstudents.net>
2026-01-29 14:33:10 +0000

    update lesson practical 3.8

--- WDLesson3_8Practical/script.js
+++ WDLesson3_8Practical/script.js
@@ -2,20 +2,25 @@
 //  For each challenge fix the error and explain the correction in the comment
 
 
-/* Challenge 4: Does the function name match the event handler referenced in the event listener? */
-function volume(){
+/* Challenge 4: Does the function name match the event handler referenced in the event listener? 
+Function name is different*/
+function cylinder(){
 
-  /* Challenge 5: Are there any errors in retrieving and parsing the information from the text inputs? */
-  let r = parseInt(document.getElementBy("r").value);
-  let height = parseInt(document.getElementbyId("height").value);
+  /* Challenge 5: Are there any errors in retrieving and parsing the information from the text inputs?
+  First line is missing 'Id' and second line the 'b' of 'by' has to be capitalize also the name should be 'h' for the id of height.*/
+  let r = parseInt(document.getElementById("r").value);
+  let h = parseInt(document.getElementById("height").value);
 
-  /* Challenge 6: Does the variable output correctly create a reference to the output container? */
-  let output = document.getElementById("output").value;
+  /* Challenge 6: Does the variable output correctly create a reference to the output container? 
+  It has '.value' that should not be there*/
+  let output = document.getElementById("output");
 
-  /* Challenge 7: Is the following calculation for volume of a cylinder correct? */
-  let v = math.PI * math.pow(r,2) / h;
+  /* Challenge 7: Is the following calculation for volume of a cylinder correct?
+  Formula is multiplying h not dividing and the 'm' for math.PI and math.pow has to be capitalize.*/
+  let v = Math.PI * Math.pow(r,2) * h;
 
-  /* Challenge 8: Are there any errors in displaying the output? */
-  output.innerHTML = `Volume of the cylinder is ${v} 
+  /* Challenge 8: Are there any errors in displaying the output? 
+  The words are missing a quote at the end of the phrase, remove the dollar sign and curly brackets and add the plus sign.*/
+  output.innerHTML = 'Volume of the cylinder is' + v; 
 
 }
