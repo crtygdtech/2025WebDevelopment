a6b0d26c407d5e0669742c86081a9188685495d3
crtygdtech <genesisd2790@nycstudents.net>
2026-04-30 03:51:42 +0000

    updates 6.7

--- WDLesson6_7Practical/script.js
+++ WDLesson6_7Practical/script.js
@@ -11,14 +11,13 @@ async function init(){
   for(let i = 0; i < data.length; i+=1){
     let complaint = data[i];
     build += `<div class="card">
-                <h2>${complaint.collision_id}</h2>
-                <h3>${complaint.crash_date}</h3>
-                <h3>${complaint.crash_time}</h3>
-                <h3>${complaint.borough}</h3>
-                <p>Language: ${country.primary_language}</p>
-                <p>Population: ${country.population}</p>
-                <p>Continent: ${country.continent}</p>
-                <a href="${country.googleMaps}" target="_blank"> Map </a>
+                <h2>Collision ID: ${complaint.collision_id}</h2>
+                <h3>Date: ${complaint.crash_date}</h3>
+                <h3>Time: ${complaint.crash_time}</h3>
+                <h3>Borough: ${complaint.borough}</h3>
+                <p>Contributing factor1: ${complaint.contributing_factor_vehicle_1}</p>
+                <p>Contributing factor2: ${complaint.contributing_factor_vehicle_2}</p>
+                <p>Contributing factor3: ${complaint.contributing_factor_vehicle_3}</p>
               </div>`
   }
 
