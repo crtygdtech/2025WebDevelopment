969752d6a13fd71577fc46e77617325cefd4cf13
crtygdtech <genesisd2790@nycstudents.net>
2026-05-04 13:24:24 +0000

    updates 6.7

--- WDLesson6_7Practical/script.js
+++ WDLesson6_7Practical/script.js
@@ -25,6 +25,32 @@ async function init(){
   output.innerHTML = build;
 
 }
+function filterByCollision_id(){
+  let output = document.getElementById("output");
+  let collision_id = document.getElementById("borough").value;
+  let result = document.getElementById("result");
+  let build = "";
+  let ct = 0;
+
+  for(let i = 0; i < data.length; i+=1){
+    let complaint = data[i];
+    if(complaint.collision_id == collision_id){
+      build += `<div class="card">
+                <h2>Collision ID: ${complaint.collision_id}</h2>
+                <h3>Date: ${complaint.crash_date}</h3>
+                <h3>Time: ${complaint.crash_time}</h3>
+                <h3>Borough: ${complaint.borough}</h3>
+                <p>Contributing factor1: ${complaint.contributing_factor_vehicle_1}</p>
+                <p>Contributing factor2: ${complaint.contributing_factor_vehicle_2}</p>
+                <p>Contributing factor3: ${complaint.contributing_factor_vehicle_3}</p>
+              </div>`
+              
+      ct += 1;
+    }
+  }
+  result.innerHTML = `${ct} Results found.`
+  output.innerHTML = build;
+}
 function filterByBorough(){
   let output = document.getElementById("output");
   let borough = document.getElementById("borough").value;
@@ -44,9 +70,36 @@ function filterByBorough(){
                 <p>Contributing factor2: ${complaint.contributing_factor_vehicle_2}</p>
                 <p>Contributing factor3: ${complaint.contributing_factor_vehicle_3}</p>
               </div>`
-  }
+              
       ct += 1;
     }
   }
   result.innerHTML = `${ct} Results found.`
   output.innerHTML = build;
+}
+function filterByBorough(){
+  let output = document.getElementById("output");
+  let borough = document.getElementById("borough").value;
+  let result = document.getElementById("result");
+  let build = "";
+  let ct = 0;
+
+  for(let i = 0; i < data.length; i+=1){
+    let complaint = data[i];
+    if(complaint.crash_date == borough){
+      build += `<div class="card">
+                <h2>Collision ID: ${complaint.collision_id}</h2>
+                <h3>Date: ${complaint.crash_date}</h3>
+                <h3>Time: ${complaint.crash_time}</h3>
+                <h3>Borough: ${complaint.borough}</h3>
+                <p>Contributing factor1: ${complaint.contributing_factor_vehicle_1}</p>
+                <p>Contributing factor2: ${complaint.contributing_factor_vehicle_2}</p>
+                <p>Contributing factor3: ${complaint.contributing_factor_vehicle_3}</p>
+              </div>`
+              
+      ct += 1;
+    }
+  }
+  result.innerHTML = `${ct} Results found.`
+  output.innerHTML = build;
+}
